f3c43443383c3f58a5bd958f50b5dab7e02ad5c6
Will Sargent <will.sargent@gmail.com>
2015-05-13 18:21:38 -0700

    Formatting fixes

--- src/main/scala/com/typesafe/play/cachecontrol/CacheControl.scala
+++ src/main/scala/com/typesafe/play/cachecontrol/CacheControl.scala
@@ -87,7 +87,8 @@ case class StoredResponse(
     status: Int,
     headers: Map[HeaderName, Seq[String]],
     requestMethod: String,
-    nominatedHeaders: Map[HeaderName, Seq[String]]) extends CacheResponse {
+    nominatedHeaders: Map[HeaderName, Seq[String]]
+) extends CacheResponse {
   import HeaderNames._
 
   lazy val directives: collection.immutable.Seq[CacheDirective] = CacheDirectiveParser.parse(headers.getOrElse(`Cache-Control`, Nil))
