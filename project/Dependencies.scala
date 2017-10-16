bb03188e9e4df537122fa7097ea016459618f8df
Marcos Pereira <marcos.silva@gmail.com>
2017-10-16 16:57:27 -0500

    Cross build to Scala 2.13.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,25 +5,25 @@ import sbt._
 
 object Dependencies {
 
-  val scalaTestVersion = "3.0.0"
+  val scalaTestVersion = "3.0.4"
 
   val scalaTest = Seq(
     "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
   )
 
-  val parserCombinatorVersion = "1.0.5"
+  val parserCombinatorVersion = "1.0.6"
   val parserCombinators = Seq(
     "org.scala-lang.modules" %% "scala-parser-combinators" % parserCombinatorVersion
   )
 
-  val slf4jVersion = "1.7.22"
+  val slf4jVersion = "1.7.25"
   val slf4j = Seq(
     "org.slf4j" % "slf4j-api" % slf4jVersion
   )
 
   val jodaTime = Seq(
-    "joda-time" % "joda-time" % "2.7",
-    "org.joda" % "joda-convert" % "1.7"
+    "joda-time" % "joda-time" % "2.9.9",
+    "org.joda" % "joda-convert" % "1.9.2"
   )
 
 }
