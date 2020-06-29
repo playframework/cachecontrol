d2fc968294b036a9196b5c9a2626e9e07f41d848
Enno Runne <458526+ennru@users.noreply.github.com>
2020-06-29 07:57:52 +0200

    scalafmtSbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,6 +16,6 @@ object Dependencies {
     "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
 
   val slf4jVersion = "1.7.30"
-  val slf4j        = "org.slf4j" % "slf4j-api" % slf4jVersion
+  val slf4j        = "org.slf4j" % "slf4j-api"    % slf4jVersion
   val slf4jSimple  = "org.slf4j" % "slf4j-simple" % slf4jVersion
 }
