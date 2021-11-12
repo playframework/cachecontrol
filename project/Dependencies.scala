42ad70e2b2cc7972373d16b46a95486abb1087eb
Scala Steward <me@scala-steward.org>
2021-11-12 22:37:06 +0100

    scala3-library 3.1.0 (was 3.0.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object Dependencies {
   val Scala212 = "2.12.14"
   val Scala213 = "2.13.6"
-  val Scala3   = "3.0.2"
+  val Scala3   = "3.1.0"
 
   val ScalaVersions = Seq(Scala212, Scala213, Scala3)
 
