872dfa6d8383dab0344166735870e0d46853a365
Scala Steward <scala_steward@virtuslab.com>
2024-10-14 19:11:37 +0000

    scala-library 2.13.15 (was 2.13.14)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object Dependencies {
   val Scala212 = "2.12.20"
-  val Scala213 = "2.13.14"
+  val Scala213 = "2.13.15"
   val Scala3   = "3.3.3"
 
   val ScalaVersions = Seq(Scala212, Scala213, Scala3)
