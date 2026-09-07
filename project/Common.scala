396fefca5b5e82d821cc6f50d548e8a9798b236c
xuwei-k <6b656e6a69@gmail.com>
2026-09-08 07:42:57 +0900

    sbt 2

--- project/Common.scala
+++ project/Common.scala
@@ -36,9 +36,9 @@ object Common extends AutoPlugin {
     Seq(
       organization         := "org.playframework",
       organizationName     := "The Play Framework Project",
-      organizationHomepage := Some(url("https://playframework.com/")),
-      homepage             := Some(url(s"https://github.com/playframework/${repoName}")),
-      licenses             := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
+      organizationHomepage := Some(uri("https://playframework.com/")),
+      homepage             := Some(uri(s"https://github.com/playframework/${repoName}")),
+      licenses             := Seq(License("Apache-2.0", uri("https://www.apache.org/licenses/LICENSE-2.0.html"))),
       scalaVersion         := Scala212,
       crossScalaVersions   := ScalaVersions,
       scalacOptions ++= scalacParameters,
@@ -47,7 +47,7 @@ object Common extends AutoPlugin {
         "playframework",
         "The Play Framework Contributors",
         "contact@playframework.com",
-        url("https://github.com/playframework")
+        uri("https://github.com/playframework")
       ),
       description := "Cachecontrol - Minimal HTTP cache management library in Scala"
     )
